20e8287401891ebd3594162846846f9e71cb773d
Joao Matos Silva <joao@kspace.pt>
2017-07-13 11:57:25 +0100

    Fix container built on app Start

--- WebApplication11/Global.asax.cs
+++ WebApplication11/Global.asax.cs
@@ -13,9 +13,13 @@ namespace WebApplication11
     {
         public const string ScopeKey = "CustomRequestScope";
 
+        private static IContainer Container { get; set; }
 
         protected void Application_Start()
         {
+            Container = BuildContainer();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(Container));
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
@@ -46,14 +50,11 @@ namespace WebApplication11
         private void ConfigureAutofac(HttpContext context)
         {
             //configure the container and start a lifetimescope
-            var container = BuildContainer();
-            var scope = container.BeginLifetimeScope(ScopeKey);
+            var scope = Container.BeginLifetimeScope(ScopeKey);
 
             //Save the scope on the context and register it to be disposed on the end of the context
             context.Items[ScopeKey] = scope;
             context.DisposeOnPipelineCompleted(scope);
-
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
